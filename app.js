55566a16b4048df19ef0db80237a098bfc9cf068
MissLiner <miss.liner@yahoo.com>
2022-10-18 13:14:45 -0400

    Add squares and button

---
+++ app.js
@@ -0,0 +1,11 @@
+const board = document.getElementById("puzzle");
+const solveButton = document.getElementById("solve-button");
+const squares = 81;
+
+for(let i = 0; i < squares; i++) {
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "number");
+  inputElement.setAttribute("min", "0");
+  inputElement.setAttribute("max", "9");
+  board.appendChild(inputElement);
+}
